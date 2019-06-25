9cb0ff867f50cd49bc4991c13428d16a3bb4ec6e
Mariana Estrada <estradacallemariana@gmail.com>
2019-06-25 18:21:00 -0500

    Calendario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'registro-e', loadChildren: './registro-e/registro-e.module#RegistroEPageModule' },
+  { path: 'inicio', loadChildren: './inicio/inicio.module#InicioPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+})
+export class MenuComponent implements OnInit {
+
+  Opciones: Opcion[] = [{
+    icon: 'person',
+    titulo: 'Paciente',
+    redirectTo: '/inicio/paciente'
+},
+{
+    icon: 'contacts',
+    titulo: 'Nosotros',
+    redirectTo: '/inicio/nosotros'
+}
+];
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
+
+export interface Opcion {
+  icon: string;
+  titulo: string;
+  redirectTo: string;
+}
